baf6bddbb9ec56659db6e423c7f83db26b23ba76
Sofia <yacumosofia@gmail.com>
2024-01-23 18:44:11 -0300

    arreglo pagina error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Footer from "./components/Footer";
 import NavbarC from "./components/NavbarC";
-import ErrorPage from "./pages/ErrorPage404";
+import ErrorPage from "./pages/ErrorPage";
 import HomePage from "./pages/HomePage";
 import RegisterPage from "./pages/RegisterPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
