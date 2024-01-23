e08a238c1dfb35cffb9b9415dafb7abe8643cb47
Sofia <yacumosofia@gmail.com>
2024-01-23 19:04:36 -0300

    cambio 404

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import Footer from "./components/Footer";
 import NavbarC from "./components/NavbarC";
-<<<<<<< HEAD
-import ErrorPage from "./pages/ErrorPage";
-=======
 import Error404Page from "./pages/Error404Page";
->>>>>>> 733cde5ee462eadee553e4a113538f0e9803eeef
 import HomePage from "./pages/HomePage";
 import RegisterPage from "./pages/RegisterPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
