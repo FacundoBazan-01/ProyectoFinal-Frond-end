a67d9a74735d8695db1580bc6200ae5ee34e357f
Sofia <yacumosofia@gmail.com>
2024-01-23 18:09:41 -0300

    cards y ad

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,22 @@ import ErrorPage from "./pages/ErrorPage";
 import HomePage from "./pages/HomePage";
 import RegisterPage from "./pages/RegisterPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProductoPage from "./pages/ProductoPage";
 
 const App = () => {
-  return (
-    <>
-      <NavbarC />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="*" element={<ErrorPage />} />
-        </Routes>
-      </BrowserRouter>
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<NavbarC />
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<HomePage />} />
+					<Route path='/register' element={<RegisterPage />} />
+					<Route path='/producto' element={<ProductoPage />} />
+					<Route path='*' element={<ErrorPage />} />
+				</Routes>
+			</BrowserRouter>
+			<Footer />
+		</>
+	);
 };
 export default App;
---
+++ src/pages/ProductoPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductoPage = () => {
+  return (
+    <div>ProductoPage</div>
+  )
+}
+
+export default ProductoPage
