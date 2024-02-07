68ae350476f3dde4e52bc8eea48cebf540b559ef
Karen <karen_micca@hotmail.com>
2024-02-07 18:42:16 -0300

    Se agrego la descripcion de cada producto y los botones para añadir al carrito y favoritos

--- src/components/DetalleCard.jsx
+++ src/components/DetalleCard.jsx
@@ -6,25 +6,40 @@ import { Link } from "react-router-dom";
 
 
 const DetalleCard= ({ producto }) => {
-	const { id, nombre, precio, img, idPage } = producto;
+	const { id, nombre, precio, img, idPage, descripcion } = producto;
 	
 	return (
 		<>
 		<div className='col-12 col-md-6 col-lg-4 '>
-			<Card className='card-class mb-5 mt-5'>
+			<Card className='card-class mb-5 mt-5 '>
+				<div className="d-flex ">
 				<Card.Img variant='top' src={img} />
 				<Card.Body>
 					<Card.Title>{nombre}</Card.Title>
 					<Card.Text> {precio} </Card.Text>
+					<Card.Text> {descripcion} </Card.Text>
 					  { idPage==='favPage'?(
+						 
          				<Link className='btn btn-danger w-100' onClick={deleteFav}>Eliminar</Link>
+						
 						): (
-							<Link to={`/producto/${id}`} className='btn btn-outline-detail '>
+							<>
+							<div className="d-flex ">
+							<Link to={`/producto/${id}`} className='btn btn-outline-detail'>
 						Añadir al carrito
-					</Link>
+					   </Link>
+					       <Link to={`/producto/${id}`} className='btn btn-outline-detail'>
+                        Añadir a Favoritos
+                      </Link>
+							</div>
+						
+					</>
 						)
+						
 					}
 				</Card.Body>
+				</div>
+				
 			</Card>
 		</div>
 		</>
