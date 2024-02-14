b325a4284fc618ac4d16ceb014000c8d49166a87
Karen <karen_micca@hotmail.com>
2024-02-14 17:07:03 -0300

    Mejoras en el codigo general de la pagina Productos,ademas se cambio el navbar

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,89 +4,20 @@ import AdPanel from "../components/AdPanel";
 import "../css/Cards.css";
 import "../pages/ProductoPage";
 import "../css/TituloG4.css";
+import agregarProductosLocalStorage from "../helper/arrayProductos";
 
 const HomePage = () => {
+
+	agregarProductosLocalStorage();
+
+	const arrayProductos = JSON.parse(localStorage.getItem("productos")) || [];
+	
 	const imgHeroPage = [
 		{
 			img1: "https://trello.com/1/cards/659c9c8986f17124996cf8ba/attachments/65c3c3ba1b95ee9c9e61c069/previews/65c3c3ba1b95ee9c9e61c0a1/download/carrousel_2.jpg",
 			img2: "https://trello.com/1/cards/659c9c8986f17124996cf8ba/attachments/65c3c3d25da8cdfbe0f1fc12/previews/65c3c3d35da8cdfbe0f1fd70/download/carrousel.jpg",
 		},
-	];
-	const arrayProductos = [
-		{
-			id: 1,
-			nombre: "Camisa blanca",
-			precio: "$4800",
-			img: "https://trello.com/1/cards/659c9c8986f17124996cf8ba/attachments/659c9cd85d82659c6ac98a29/previews/659c9cd95d82659c6ac98ed6/download/8372060250_2_1_1.jpg",
-		},
-		{
-			id: 2,
-			nombre: "Remera un hombro",
-			precio: "$7500",
-			img: "https://trello.com/1/cards/659c9c8986f17124996cf8ba/attachments/659c9cd7e64462e9d143cdbc/previews/659c9cd7e64462e9d143cdfe/download/5644858251_2_1_1.jpg",
-		},
-		{
-			id: 3,
-			nombre: "Chaleco lana gris",
-			precio: "$5500",
-			img: "https://trello.com/1/cards/659c9c8986f17124996cf8ba/attachments/659c9cd520d6314f0674950b/previews/659c9cd620d6314f0674955a/download/5536157803_2_1_1.jpg",
-		},
-		{
-			id: 4,
-			nombre: "Campera de cuero",
-			precio: "$15000",
-			img: "https://trello.com/1/cards/659c9c8986f17124996cf8ba/attachments/659c9cd49d6145864f4f66eb/previews/659c9cd59d6145864f4f6744/download/5427413803_2_1_1.jpg",
-		},
-		{
-			id: 5,
-			nombre: "Vestido negro",
-			precio: "$18000",
-			img: "https://trello.com/1/cards/659c9c8986f17124996cf8ba/attachments/659c9cd2370a6c249fd916c8/previews/659c9cd3370a6c249fd9180a/download/3067279800_2_1_1.jpg",
-		},
-
-		{
-			id: 6,
-			nombre: "Pollera cuero tiro alto",
-			precio: "$9000",
-			img: "https://trello.com/1/cards/659c9c8986f17124996cf8ba/attachments/659c9cd13d9b48300df28dc4/previews/659c9cd23d9b48300df28def/download/3046246800_2_2_1.jpg",
-		},
-		{
-			id: 7,
-			nombre: "Vestido Minmay",
-			precio: "$8000",
-			img: "https://trello.com/1/cards/659c9c8986f17124996cf8ba/attachments/659c9cd0fac4f9490f9ccfbc/previews/659c9cd1fac4f9490f9ccfd4/download/2245717507_2_1_1.jpg",
-		},
-		{
-			id: 8,
-			nombre: "Vestido largo color hueso",
-			precio: "$7500",
-			img: "https://trello.com/1/cards/659c9c8986f17124996cf8ba/attachments/659c9ccf6209b0f619971961/previews/659c9cd06209b0f61997198b/download/2149010710_2_1_1.jpg",
-		},
-		{
-			id: 9,
-			nombre: "Buzo gris lana",
-			precio: "$5500",
-			img: "https://trello.com/1/cards/659c9c8986f17124996cf8ba/attachments/659c9cce9d6145864f4f57c9/previews/659c9cce9d6145864f4f5ae3/download/2144187800_2_3_1.jpg",
-		},
-		{
-			id: 10,
-			nombre: "Pollera tableada tiro bajo",
-			precio: "$8000",
-			img: "https://trello.com/1/cards/659c9c8986f17124996cf8ba/attachments/659c9ccb0f7190eeeb4aed51/previews/659c9ccc0f7190eeeb4aed64/download/2055583800_2_2_1.jpg",
-		},
-		{
-			id: 11,
-			nombre: "Short cuero tiro alto",
-			precio: "$5500",
-			img: "https://trello.com/1/cards/659c9c8986f17124996cf8ba/attachments/659c9ccaef1ca4530cdfb2fa/previews/659c9ccbef1ca4530cdfb314/download/8372322800_2_1_1.jpg",
-		},
-		{
-			id: 12,
-			nombre: "Camisa top blanca",
-			precio: "$4500",
-			img: "https://trello.com/1/cards/659c9c8986f17124996cf8ba/attachments/659c9ccc85db741c6d1aba8d/previews/659c9ccd85db741c6d1abaff/download/2131231250_2_1_1.jpg",
-		},
-	];
+	]
 	return (
 		<>
 			{imgHeroPage.map((producto) => (
@@ -108,6 +39,5 @@ const HomePage = () => {
 			</div>
 		</>
 	);
-};
-
-export default HomePage;
+}
+export default HomePage
--- src/pages/ProductoPage.jsx
+++ src/pages/ProductoPage.jsx
@@ -1,124 +1,53 @@
-import React, { useEffect, useState } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import React from 'react';
 import { useParams } from 'react-router-dom';
-import DetalleCard from '../components/DetalleCard';
-
-const arrayProductos = [
-  {
-    id: 1,
-    nombre: "Camisa blanca",
-    descripcion: "Camisa confeccionada con tejido en mezcla de algodón. Cuello solapa con escote pico y manga larga. Cierre frontal con botones.",
-    precio: "$4800",
-    img: "../helps/imagenesProductos/producto_N°1.jpg",
-    
-  },
-  {
-    id: 2,
-    nombre: "Remera un hombro ",
-    descripcion: "Camiseta de escote asimétrico y tirante ancho. Detalle de abertura en espalda.",
-    precio: "$7500",
-    img: "https://trello.com/1/cards/659c9c8986f17124996cf8ba/attachments/659c9cd7e64462e9d143cdbc/previews/659c9cd7e64462e9d143cdfe/download/5644858251_2_1_1.jpg",
-  },
-  {
-    id: 3,
-    nombre: "Chaleco lana gris",
-    descripcion: "Chaleco de escote pico y manga sisa. Bajo con aberturas laterales.",
-    precio: "$5500",
-    img: "https://trello.com/1/cards/659c9c8986f17124996cf8ba/attachments/659c9cd520d6314f0674950b/previews/659c9cd620d6314f0674955a/download/5536157803_2_1_1.jpg",
-  },
-  {
-    id: 4,
-    nombre: "Campera de cuero",
-    descripcion: "Cazadora corta confeccionada con lino y viscosa. Cuello solapa y manga larga acabada en vuelta. Bolsillos delanteros de plastrón. Cierre frontal con botones.",
-    precio: "$15000",
-    img: "https://trello.com/1/cards/659c9c8986f17124996cf8ba/attachments/659c9cd49d6145864f4f66eb/previews/659c9cd59d6145864f4f6744/download/5427413803_2_1_1.jpg",
-  },
-  {
-    id: 5,
-    nombre: "Vestido negro",
-    descripcion: "Vestido de escote pico y manga larga. Detalle de cinturón fino.",
-    precio: "$18000",
-    img: "https://trello.com/1/cards/659c9c8986f17124996cf8ba/attachments/659c9cd2370a6c249fd916c8/previews/659c9cd3370a6c249fd9180a/download/3067279800_2_1_1.jpg",
-  },
-
-  {
-    id: 6,
-    nombre: "Pollera cuero tiro alto",
-    descripcion: "Falda corta de tiro alto. Cierre lateral con cremallera oculta en costura.",
-    precio: "$9000",
-    img: "https://trello.com/1/cards/659c9c8986f17124996cf8ba/attachments/659c9cd13d9b48300df28dc4/previews/659c9cd23d9b48300df28def/download/3046246800_2_2_1.jpg",
-  },
-  {
-    id: 7,
-    nombre: "Vestido Minmay",
-    descripcion: "Vestido mini de cuello mao y manga corta. Detalle de bordados combinados a tono",
-    precio: "$8000",
-    img: "https://trello.com/1/cards/659c9c8986f17124996cf8ba/attachments/659c9cd0fac4f9490f9ccfbc/previews/659c9cd1fac4f9490f9ccfd4/download/2245717507_2_1_1.jpg",
-  },
-  {
-    id: 8,
-    nombre: "Vestido largo color hueso",
-    descripcion: "Vestido midi de escote pico y tirantes. Tejido con textura en relieve.",
-    precio: "$7500",
-    img: "https://trello.com/1/cards/659c9c8986f17124996cf8ba/attachments/659c9ccf6209b0f619971961/previews/659c9cd06209b0f61997198b/download/2149010710_2_1_1.jpg",
-  },
-  {
-    id: 9,
-    nombre: "Buzo gris lana",
-    descripcion: "Buzo con tejido en mezcla de lana. Escote pico y manga larga. Cierre en diagonal con botones.",
-    precio: "$5500",
-    img: "https://trello.com/1/cards/659c9c8986f17124996cf8ba/attachments/659c9cce9d6145864f4f57c9/previews/659c9cce9d6145864f4f5ae3/download/2144187800_2_3_1.jpg",
-  },
-  {
-    id: 10,
-    nombre: "Pollera tableada tiro bajo",
-    descripcion: "Falda con cinturilla elástica con cierre botón frontal y aplique cinturón ajustable. Detalle tablas.",
-    precio: "$8000",
-    img: "https://trello.com/1/cards/659c9c8986f17124996cf8ba/attachments/659c9ccb0f7190eeeb4aed51/previews/659c9ccc0f7190eeeb4aed64/download/2055583800_2_2_1.jpg",
-  },
-  {
-    id: 11,
-    nombre: "Short cuero tiro alto",
-    descripcion: "Bermuda de tiro alto con pinzas delanteras tiro alto, Bolsillos delanteros y falso bolsillo ",
-    precio: "$5500",
-    img: "https://trello.com/1/cards/659c9c8986f17124996cf8ba/attachments/659c9ccaef1ca4530cdfb2fa/previews/659c9ccbef1ca4530cdfb314/download/8372322800_2_1_1.jpg",
-  },
-  {
-    id: 12,
-    nombre: "Camisa top blanca",
-    descripcion: "Top con cuello y manga corta acabada en elástico. Detalle de acabados con volantes y encaje.",
-    precio: "$4500",
-    img: "https://trello.com/1/cards/659c9c8986f17124996cf8ba/attachments/659c9ccc85db741c6d1aba8d/previews/659c9ccd85db741c6d1abaff/download/2131231250_2_1_1.jpg",
-  },
-];
 const ProductoPage = () => {
   const params= useParams();
-  const [producto,setProducto] = useState ();
-
-  //const getProducto = async ()=> {
-    //const OneProducto = await fetch (`http://localhost:5173/producto/${params.id}`);
+  const { id } = params; 
+  const arrayProductos = JSON.parse(localStorage.getItem("productos")) || [];
+  const prodFilter = arrayProductos.filter((producto) => producto.id === Number(id) );
+  const agregarProducto = (id) => {
+   const cartLs = JSON.parse(localStorage.getItem("cart")) || [];
+   if(cartLs.length > 0) {
+    const prodExistCart = cartLs.filter(
+      (producto) => producto.id === Number(id)
+     );
+     if (prodExistCart.length === 0){
+      cartLs.push(prodFilter[0]);
+      localStorage.setItem("cart", JSON.stringify(cartLs));
+      return;
+     } else{
+      alert ("El producto ya existe en el carrito");
+     }
+    
+   } else{
+    cartLs.push(prodFilter[0]);
+    localStorage.setItem("cart", JSON.stringify(cartLs));
+   }
+  };
  
-  //setProducto (OneProducto.data.getAllProductoOne)
-  //}
-  useEffect (() => {
-   // getProducto();
-    setProducto(arrayProductos.filter(p => p.id == params.id))
-    console.log(arrayProductos);
-  },[]);
   return (
     <>
-    <Container>
-      <Row>
-      {producto?.map((p) => (
-      <Col>
-      	<DetalleCard
-           producto ={p}
-            />
-      </Col>
-					))}
-      </Row>
-    </Container>
-    </>
+    {
+       prodFilter.map((producto) => 
+
+       <div className='d-flex justify-content-center' key={producto.id}>
+          
+          <div className='w-25'>
+            <img src={producto.img} alt="" width={"100%"} />
+          </div>
+          <div>
+              <p>{producto.nombre}</p>
+              <p>{producto.precio}</p>
+
+              <div>
+                <button className='btn btn-success me-2' onClick={() => agregarProducto(producto.id) }>Añadir al carrito</button>
+                <button className='btn btn-danger'>Añadir a favoritos</button>
+              </div>
+          </div>
+       </div>
+       )
+    }
+   </>
   );
 };
 
