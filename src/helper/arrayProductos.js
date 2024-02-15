80d15db0265d5b255fe29f32712a5849495716cd
Karen <karen_micca@hotmail.com>
2024-02-14 21:24:47 -0300

    Agregue estilos en ProductoPage

--- src/helper/arrayProductos.js
+++ src/helper/arrayProductos.js
@@ -6,30 +6,35 @@ const agregarProductosLocalStorage = () =>
             nombre: "Camisa blanca",
             precio: "$4800",
             img: "https://trello.com/1/cards/659c9c8986f17124996cf8ba/attachments/659c9cd85d82659c6ac98a29/previews/659c9cd95d82659c6ac98ed6/download/8372060250_2_1_1.jpg",
+            descripcion:"Camisa confeccionada con tejido en mezcla de algodón. Cuello solapa con escote pico y manga larga. Cierre frontal con botones."
         },
         {
             id: 2,
             nombre: "Remera un hombro",
             precio: "$7500",
             img: "https://trello.com/1/cards/659c9c8986f17124996cf8ba/attachments/659c9cd7e64462e9d143cdbc/previews/659c9cd7e64462e9d143cdfe/download/5644858251_2_1_1.jpg",
+            descripcion:"Camiseta de escote asimétrico y manga corta."
         },
         {
             id: 3,
             nombre: "Chaleco lana gris",
             precio: "$5500",
             img: "https://trello.com/1/cards/659c9c8986f17124996cf8ba/attachments/659c9cd520d6314f0674950b/previews/659c9cd620d6314f0674955a/download/5536157803_2_1_1.jpg",
+            descripcion:"Chaleco con escote pico y manga sisa. Falsos bolsillos delanteros de vivo."
         },
         {
             id: 4,
             nombre: "Campera de cuero",
             precio: "$15000",
             img: "https://trello.com/1/cards/659c9c8986f17124996cf8ba/attachments/659c9cd49d6145864f4f66eb/previews/659c9cd59d6145864f4f6744/download/5427413803_2_1_1.jpg",
+            descripcion:"Cazadora de cuello solapa y manga larga con trabillas en hombro. Bolsillos delanteros con cremallera metálica."
         },
         {
             id: 5,
             nombre: "Vestido negro",
             precio: "$18000",
             img: "https://trello.com/1/cards/659c9c8986f17124996cf8ba/attachments/659c9cd2370a6c249fd916c8/previews/659c9cd3370a6c249fd9180a/download/3067279800_2_1_1.jpg",
+            descripcion:"Vestido de cuello redondo y manga larga. Cierre en espada con cremallera."
         },
     
         {
@@ -37,42 +42,49 @@ const agregarProductosLocalStorage = () =>
             nombre: "Pollera cuero tiro alto",
             precio: "$9000",
             img: "https://trello.com/1/cards/659c9c8986f17124996cf8ba/attachments/659c9cd13d9b48300df28dc4/previews/659c9cd23d9b48300df28def/download/3046246800_2_2_1.jpg",
+            descripcion:"Falda de cuero de tiro alto. Falsos bolsillos de vivo en delantero. Cierre lateral con cremallera oculta en costura."
         },
         {
             id: 7,
             nombre: "Vestido Minmay",
             precio: "$8000",
             img: "https://trello.com/1/cards/659c9c8986f17124996cf8ba/attachments/659c9cd0fac4f9490f9ccfbc/previews/659c9cd1fac4f9490f9ccfd4/download/2245717507_2_1_1.jpg",
+            descripcion:"Vestido de cuello redondo y manga sisa. Detalle de espalda descubierta. Cierre en espalda con botón."
         },
         {
             id: 8,
             nombre: "Vestido largo color hueso",
             precio: "$7500",
             img: "https://trello.com/1/cards/659c9c8986f17124996cf8ba/attachments/659c9ccf6209b0f619971961/previews/659c9cd06209b0f61997198b/download/2149010710_2_1_1.jpg",
+            descripcion:"Vestido de escote pico con cuello.Manga larga"
         },
         {
             id: 9,
             nombre: "Buzo gris lana",
             precio: "$5500",
             img: "https://trello.com/1/cards/659c9c8986f17124996cf8ba/attachments/659c9cce9d6145864f4f57c9/previews/659c9cce9d6145864f4f5ae3/download/2144187800_2_3_1.jpg",
+            descripcion:"Jersey de cuello redondo y manga larga de lana."
         },
         {
             id: 10,
             nombre: "Pollera tableada tiro bajo",
             precio: "$8000",
             img: "https://trello.com/1/cards/659c9c8986f17124996cf8ba/attachments/659c9ccb0f7190eeeb4aed51/previews/659c9ccc0f7190eeeb4aed64/download/2055583800_2_2_1.jpg",
+            descripcion:"Falda tipo skort cinturilla elástica con cierre botón frontal y aplique. Detalle tablas."
         },
         {
             id: 11,
             nombre: "Short cuero tiro alto",
             precio: "$5500",
             img: "https://trello.com/1/cards/659c9c8986f17124996cf8ba/attachments/659c9ccaef1ca4530cdfb2fa/previews/659c9ccbef1ca4530cdfb314/download/8372322800_2_1_1.jpg",
+            descripcion:"Bermudas de cuero tiro alto con bolsillos en delantero. Cierre lateral con cremallera. Cierre lateral con cremallera oculta en costura."
         },
         {
             id: 12,
             nombre: "Camisa top blanca",
             precio: "$4500",
             img: "https://trello.com/1/cards/659c9c8986f17124996cf8ba/attachments/659c9ccc85db741c6d1aba8d/previews/659c9ccd85db741c6d1abaff/download/2131231250_2_1_1.jpg",
+            descripcion:"Camisa corta de cuello solapa y manga sisa amplia. Bajo ajustable con cordones en mismo tejido. Cierre frontal con botones."
         },
     ];
     
--- src/pages/ProductoPage.jsx
+++ src/pages/ProductoPage.jsx
@@ -1,54 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import ToggleButton from 'react-bootstrap/ToggleButton';
+import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup';
+import "../css/Producto.css";
+
 const ProductoPage = () => {
-  const params= useParams();
-  const { id } = params; 
+  const params = useParams();
+  const { id } = params;
   const arrayProductos = JSON.parse(localStorage.getItem("productos")) || [];
-  const prodFilter = arrayProductos.filter((producto) => producto.id === Number(id) );
+  const prodFilter = arrayProductos.filter((producto) => producto.id === Number(id));
+
   const agregarProducto = (id) => {
-   const cartLs = JSON.parse(localStorage.getItem("cart")) || [];
-   if(cartLs.length > 0) {
-    const prodExistCart = cartLs.filter(
-      (producto) => producto.id === Number(id)
-     );
-     if (prodExistCart.length === 0){
-      cartLs.push(prodFilter[0]);
-      localStorage.setItem("cart", JSON.stringify(cartLs));
-      return;
-     } else{
-      alert ("El producto ya existe en el carrito");
-     }
-    
-   } else{
-    cartLs.push(prodFilter[0]);
-    localStorage.setItem("cart", JSON.stringify(cartLs));
-   }
+    // ... (tu lógica de agregar al carrito)
   };
- 
-  return (
-    <>
-    {
-       prodFilter.map((producto) => 
 
-       <div className='d-flex justify-content-center' key={producto.id}>
-          
-          <div className='w-25'>
-            <img src={producto.img} alt="" width={"100%"} />
-          </div>
-          <div>
-              <p>{producto.nombre}</p>
-              <p>{producto.precio}</p>
+  const ToggleButtonGroupControlled = () => {
+    const [value, setValue] = useState([1, 4]);
 
-              <div>
-                <button className='btn btn-success me-2' onClick={() => agregarProducto(producto.id) }>Añadir al carrito</button>
-                <button className='btn btn-danger'>Añadir a favoritos</button>
+    const handleChange = (val) => setValue(val);
+
+    return (
+      <React.Fragment>
+        {
+          prodFilter.map((producto) =>
+            <div className='container mt-5' key={producto.id}>
+              <div className='row justify-content-center'>
+                <div className='col-12 col-sm-6 col-md-4 me-5'>
+                  <img src={producto.img} alt="" className="img-fluid" />
+                </div>
+                <div className='col-12 col-sm-6 col-md-4 borde-cuadro '>
+                  <p className='titulo-mayuscula'>{producto.nombre}</p>
+                  <p className='color-precio'>{producto.precio} ARS</p>
+                  <p>{producto.descripcion}</p>
+                  <hr />
+                  <div className='text-center'>
+                    <h5>SELECCIONA TU TALLE</h5>
+                  <ToggleButtonGroup type="checkbox" value={value} onChange={handleChange}>
+                    <div className='estilo-botones'>
+                    <ToggleButton id="tbg-btn-1" value={1}>
+                      S
+                    </ToggleButton>
+                    </div>
+                    <div className='estilo-botones'>
+                    <ToggleButton id="tbg-btn-2" value={2}>
+                      L
+                    </ToggleButton>
+                    </div>
+                    <div className='estilo-botones'>
+                    <ToggleButton id="tbg-btn-3" value={3}>
+                      M
+                    </ToggleButton>
+                    </div>
+                    <div className='estilo-botones'>
+                    <ToggleButton id="tbg-btn-3" value={4}>
+                      XL
+                    </ToggleButton>
+                    </div>
+                  </ToggleButtonGroup>
+                  </div>
+                 
+                  <div className='text-center mt-5 '>
+                    <button className='btn estilo-botonAñadir1 me-2' onClick={() => agregarProducto(producto.id)}>Añadir al carrito</button>
+                    <button className='btn estilo-botonAñadir2'>Añadir a favoritos</button>
+                  </div>
+                </div>
               </div>
-          </div>
-       </div>
-       )
-    }
-   </>
-  );
+            </div>
+          )
+        }
+      </React.Fragment>
+    );
+  };
+
+  return <ToggleButtonGroupControlled />;
 };
 
 export default ProductoPage;
+
