68acb6adcbf64a8743ff174059ae08ae94f808d9
Contipenalva <139813750+Contipenalva@users.noreply.github.com>
2024-02-29 21:12:18 -0300

    creo favoritos

--- src/pages/FavPage.jsx
+++ src/pages/FavPage.jsx
@@ -1,41 +1,23 @@
-import React, { useState } from 'react';
-import CardsC from '../components/CardsC';
-import '../css/Cards.css';
+import React from "react";
+import CardsC from "../components/CardsC";
 
 const FavPage = () => {
-  const [favoritos, setFavoritos] = useState([]);
-
-  //const getAllFav = async () => {
-    // try {
-      //const favs = await clienteAxios.get("/favs", configHeaders);
-      //console.log(favs.data.getFavs[0].favoritos)
-      //setFavoritos(favs.data.getFavs[0].favoritos);
-    //} catch (error) {
-      //console.log(error);
-    //}
-  //};
-
-  //useEffect(() => {
-    //getAllFav();
-  //}, []);
-
-  const handleEliminar = (id) => {
-    const nuevosFavoritos = favoritos.filter((producto) => producto.id !== id);
-    setFavoritos(nuevosFavoritos);
-  };
 
   return (
     <>
-      <div className='container mt-5 mb-5'>
-        <div className='row'>
-          {favoritos.map((producto) => (
-            <CardsC
-              producto={producto}
-              key={producto.id}
-              onDelete={handleEliminar}
-              idPage='favPage'
-            />
-          ))}
+      <div className="container">
+        <div className="row">
+          {favLS.length > 0 ? (
+            favLS.map((producto) => (
+              <div className="col-12 col-md-6 col-lg-4" key={producto.id}>
+                <CardsC producto={producto} idComponent="favoritos" />
+              </div>
+            ))
+          ) : (
+            <h1 className="text-center py-5">
+              No hay productos en Favoritos por el momento!!!!
+            </h1>
+          )}
         </div>
       </div>
     </>
