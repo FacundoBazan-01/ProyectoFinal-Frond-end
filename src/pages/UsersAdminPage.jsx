f96bcd12ee64e88fa6c147634eaafd609f34368c
Contipenalva <139813750+Contipenalva@users.noreply.github.com>
2024-03-02 19:52:18 -0300

    agrego modal para admin

--- src/pages/UsersAdminPage.jsx
+++ src/pages/UsersAdminPage.jsx
@@ -1,10 +1,27 @@
+import {useState} from 'react';
 import React from 'react'
 import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+
+
 
 const AdminUserPage = () => {
 
     const arrarUsersLocalStorage = JSON.parse(localStorage.getItem('Usuarios')) || [];
 
+    const [show, setShow] = useState(false);
+    
+    const [dataUser, setDataUser] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = (ev, usuario) => {
+      ev.preventDefault
+      setDataUser(usuario)
+      setShow(true);
+    }
+    
   return (
     <>
 
@@ -19,17 +36,44 @@ const AdminUserPage = () => {
       </thead>
       <tbody>
         { arrarUsersLocalStorage.map((usuario) => (
-          <tr>
+          <tr key={usuario.id}>
           <td>{usuario.id}</td>
           <td>{usuario.nombre}</td>
           <td>{usuario.role}</td>
           <td>
-            <button>
+            <button className='btn estilo-botonAñadir1 mx-2'>
+              Eliminar
+            </button>
+            <Button className='btn estilo-botonAñadir1' onClick={(ev) => handleShow(ev, usuario)}>
+        Editar
+      </Button>
 
-            </button>
-            <button>
-              
-            </button>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Editar Usuario</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Usuario</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" value={dataUser?.nombre} 
+        onClick={(ev) => handleShow(ev)}
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Role</Form.Label>
+        <Form.Control type="text" placeholder="Password" value={dataUser?.role} />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Enviar
+      </Button>
+    </Form>
+        </Modal.Body>
+      </Modal>
           </td>
         </tr>
         )
